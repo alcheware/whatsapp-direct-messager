f7a1cca905b995893d555fbbda4b7fbd81da19f4
Gökdeniz Özkan <gokdeniz@gokdenizozkan.com>
2024-01-17 13:41:27 +0300

    fix bugs and improve - find to includes & keydown to keyup - v0.1.2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const form_url = (phone_number) => {
     let url = "https://wa.me/";
-    return phone_number.find("+") ? url + phone_number : url + "+" + phone_number;
+    return phone_number.includes("+") ? url + phone_number : url + "+" + phone_number;
 }
 const on_keyup = (phone_number_element, link_element) => {
     let url = form_url(phone_number_element.value);
@@ -11,5 +11,5 @@ const on_keyup = (phone_number_element, link_element) => {
 window.onload = () => {
     let input = document.getElementById("phone-number");
     let url_element = document.getElementById("url");
-    input.addEventListener("keydown", (e) => {on_keyup(input, url_element)});
+    input.addEventListener("keyup", (e) => {on_keyup(input, url_element)});
 }
