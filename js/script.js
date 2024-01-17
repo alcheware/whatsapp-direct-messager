f7d79ec1d0ae5a8fa0ad75bced01ad80ff733dd4
Gökdeniz Özkan <gokdeniz@gokdenizozkan.com>
2024-01-17 13:37:38 +0300

    add 'adapt form_url() to different inputs'

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-const form_url = (phone_number) => {return "https://wa.me/" + phone_number;}
+const form_url = (phone_number) => {
+    let url = "https://wa.me/";
+    return phone_number.find("+") ? url + phone_number : url + "+" + phone_number;
+}
 const on_keyup = (phone_number_element, link_element) => {
     let url = form_url(phone_number_element.value);
     link_element.setAttribute("href", url);
