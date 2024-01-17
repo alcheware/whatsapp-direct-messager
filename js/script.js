61dba552ab77b525efc311abab994770191b6dbb
Gökdeniz Özkan <gokdeniz@gokdenizozkan.com>
2024-01-17 14:17:58 +0300

    add input validity check - v0.1.3

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-const form_url = (phone_number) => {
+function form_url(phone_number) {
     let url = "https://wa.me/";
     return phone_number.includes("+") ? url + phone_number : url + "+" + phone_number;
 }
-const on_keyup = (phone_number_element, link_element) => {
-    let url = form_url(phone_number_element.value);
+
+function on_keyup(phone_number_element, link_element) {
+    let input = phone_number_element.value;
+    let invalid = false;
+    let error_regex = /[^+0-9]+/;
+    error_regex.test(input) || input === "" ? invalid = true : invalid = false;
+
+    if (invalid) {
+        phone_number_element.setAttribute("class", "number-error");
+        link_element.setAttribute("href", "#");
+        link_element.innerHTML = "Invalid phone number";
+        return;
+    }
+
+    phone_number_element.setAttribute("class", "number-ok");
+    let url = form_url(input);
     link_element.setAttribute("href", url);
     link_element.innerHTML = url;
 }
